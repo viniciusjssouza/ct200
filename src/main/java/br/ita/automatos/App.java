dc069f3b63b08da41e792fc7e52bd057587723cd
Vinicius Souza <viniciusjssouza@gmail.com>
2016-09-18 22:35:23 -0300

    Criacao do projeto para o trabalho 1

---
+++ src/main/java/br/ita/automatos/App.java
@@ -0,0 +1,13 @@
+package br.ita.automatos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
